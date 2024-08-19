e03bfd82f6e3e79005f1156626a3d23b46a0e762
Ashin Johnson <ashinjohn33@gmail.com>
2024-08-19 20:06:48 +0530

    Learned about comparison operators and datatypes

---
+++ comp.js
@@ -0,0 +1,17 @@
+let x = 10;
+let y = 20;
+
+console.log(x >= y);
+
+result = x > y ? x : y;
+
+console.log(result);
+
+a = true;
+b = false;
+
+console.log(a && b);
+console.log(a || b);
+console.log(a && !b);
+
+console.log(BigInt(123456789987654321456987))
